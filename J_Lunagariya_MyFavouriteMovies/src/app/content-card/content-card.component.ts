4d327410c734cd3f0e6ac72ced390dccfbaa43db
jaylu <jaylunagariya7903@gmail.com>
2023-02-24 20:15:58 -0500

    added assignment 1

---
+++ J_Lunagariya_MyFavouriteMovies/src/app/content-card/content-card.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ContentList } from '../helper-files/content-list';
+import { Content } from '../helper-files/content-interface';
+
+
+function constructor() {
+
+  // @ts-ignore
+  this.contentList = new ContentList();
+  {
+    const contentItems: Content[] = [
+      { id: 1, title: 'Love The Way You Lie', description: 'Favourite Song', creator: 'Rihana' },
+      { id: 2, title: 'Baby', description: 'it has record for most viewers', creator: 'Salman Khan' },
+      { id: 3, title: 'Come Around me', description: 'this song is very popular', creator: 'Alex' }
+    ];
+
+    // @ts-ignore
+    contentItems.forEach(item => this.contentList.add(item));
+
+  }
+}
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.css']
+})
+export class ContentCardComponent {
+  get contentList(): any {
+    return this._contentList;
+  }
+
+  set contentList(value: any) {
+    this._contentList = value;
+  }
+  private _contentList: any;
+  item: any;
+
+}
---
+++ J_Lunagariya_MyFavouriteMovies/src/app/helper-files/content-list.ts
@@ -0,0 +1,44 @@
+import { Content } from "../helper-files/content-interface";
+
+export class ContentList {
+  private readonly contents: Content[];
+  items: any;
+
+  constructor() {
+    this.contents = [];
+  }
+
+  getContents(): Content[] {
+    return this.contents;
+  }
+
+  add(content: Content) {
+    this.contents.push(content);
+  }
+
+  length(): number {
+    return this.contents.length;
+  }
+
+  getHtml(index: number): string {
+    if (index < 0 || index >= this.items.length) {
+      return '<p>Error: index out of range</p>';
+    }
+    const item = this.items[index];
+
+    const content = this.contents[index];
+    const title = content.title;
+    const description = content.description;
+    const creator = content.creator;
+    const imgURL = content.imgURL;
+    const type = content.type;
+    let html = `<p>Title: ${title}</p><p>Description: ${description}</p><p>Creator: ${creator}</p>`;
+    if (imgURL) {
+      html += `<img src='${imgURL}' alt='${type}'>`;
+    }
+    if (type) {
+      html += `<p>Type: ${type}</p>`;
+    }
+    return html;
+  }
+}
